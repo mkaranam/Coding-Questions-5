07c733826e617048ce3b147faa7e42cb36cf5afe
mkaranam <karanam.manikanta@gmail.com>
2014-07-06 23:49:48 -0500

    Coding questions part 5
    Useful for top coder challenges, interviews and just fun!!

---
+++ Recursion.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    class Program
+    {
+        public int BinarySearch(int[] input,int key)
+        {
+            if (input.Length == 0) return -1;
+            return BS(key, input, 0, input.Length - 1);
+        }
+
+        private int BS(int key, int[] input, int low, int high)
+        {
+            if (low > high) return -1;
+            int mid = (low + high) / 2;
+            if (input[mid] == key) return mid;
+            if (key < input[mid]) return BS(key, input, low, mid - 1);
+            else return BS(key, input, mid + 1, high);
+        }
+
+        public void printPermutations(String input)
+        {
+            if (input.Length == 0) return;
+            if (input.Length == 1)
+            {
+                Console.WriteLine(input[0]);
+                return;
+            }
+            StringBuilder sb = new StringBuilder();
+            bool[] inPermut = new bool[input.Length];
+            for (int i = 0; i < input.Length; i++) inPermut[i] = false;
+
+            printStringPermutations(input.ToCharArray(), sb, inPermut);
+        }
+
+        private void printStringPermutations(char[] input, StringBuilder sb, bool[] inPermut)
+        {
+            if (sb.Length == input.Length)
+            {
+                Console.WriteLine(sb.ToString());
+                return;
+            }
+
+            int[] candidates = getCandidates(input, inPermut);
+            for (int i = 0; i < candidates.Length; i++)
+            {
+                sb.Append(input[candidates[i]]);    //Add to string
+                inPermut[candidates[i]] = true;                 //Mark as in string
+                printStringPermutations(input, sb, inPermut);   //Continue building string
+                sb.Remove(sb.Length - 1 , 1);
+                inPermut[candidates[i]] = false;
+            }
+        }
+
+        private int[] getCandidates(char[] input, bool[] inPermut)
+        {
+            List<int> candidates = new List<int>();
+            for (int i = 0; i < inPermut.Length; i++)
+            {
+                if (!inPermut[i]) candidates.Add(i);
+            }
+            return candidates.ToArray();
+        }
+
+        public void printCombinations(String input)
+        {
+            if (input.Length == 0) return;
+
+            StringBuilder sb = new StringBuilder();
+            bool[] inPermut = new bool[input.Length];
+            for (int i = 0; i < input.Length; i++) inPermut[i] = false;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                sb.Append(input[i]);
+                inPermut[i] = true;
+                printStringCombinations(input.ToCharArray(), sb, inPermut, input.Length - i, i);
+                sb.Clear();
+            }
+        }
+
+        private void printStringCombinations(char[] input, StringBuilder sb, bool[] inPermut, int targetLength, int start)
+        {
+            if (targetLength == 0)
+            {
+                return;
+            }
+            targetLength--;
+            for (int i = start; i < input.Length; i++)
+            {
+                if (!inPermut[i])
+                {
+                    sb.Append(input[i]);
+                    inPermut[i] = true;
+                    printStringCombinations(input, sb, inPermut, targetLength, i);
+                    sb.Remove(sb.Length - 1, 1);
+                    inPermut[i] = false;
+                }
+            }
+            Console.WriteLine(sb.ToString());
+        }
+
+        public void printPhoneNumberCombination(int[] input)
+        {
+            if (input.Length == 0) return;
+            StringBuilder sb = new StringBuilder();
+            printPhoneNumber(input, sb, 0);
+        }
+
+        private void printPhoneNumber(int[] input, StringBuilder sb, int index)
+        {
+            if (sb.Length == input.Length)
+            {
+                Console.WriteLine(sb.ToString());
+                return;
+            }
+            char[] dialPad = getDialPad(input[index]);
+            index++;
+            if (dialPad.Length <= 0)
+            {
+                sb.Append(input[index]);
+                printPhoneNumber(input, sb, index);
+                sb.Remove(sb.Length - 1, 1);
+            }
+            else 
+            {
+                for (int i = 0; i < dialPad.Length; i++)
+                {
+                    sb.Append(dialPad[i]);
+                    printPhoneNumber(input, sb, index);
+                    sb.Remove(sb.Length - 1, 1);
+                }
+            }
+            
+        }
+
+        private char[] getDialPad(int number)
+        {
+            switch (number)
+            {
+                case 2:
+                    return new char[] { 'A', 'B', 'C' };
+                case 3:
+                    return new char[] { 'D', 'E', 'F' };
+                case 4:
+                    return new char[] { 'G', 'H', 'I' };
+                case 5:
+                    return new char[] { 'J', 'K', 'L' };
+                case 6:
+                    return new char[] { 'M', 'N', 'O' };
+                case 7:
+                    return new char[] { 'P', 'R', 'S' };
+                case 8:
+                    return new char[] { 'T', 'U', 'V' };
+                case 9:
+                    return new char[] { 'W', 'X', 'Y' };
+                default:
+                    return new char[] { };
+            }
+        }
+
+    }
+}
---
+++ Strings.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysAndStrings
+{
+    class Strings
+    {
+
+        public char? firstNonRepeatedChar(string input)
+        {
+            int[] charSet = new int[256];
+            for (int i = 0; i < 256; i++) charSet[i] = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = input.ElementAt(i);
+                charSet[c]++;
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = input.ElementAt(i);
+                if (charSet[c] == 1) return c;
+            }
+            return null;
+        }
+
+        public string removeChars(string input, string remove)
+        {
+            if (input.Length == 0 || remove.Length == 0) return input;
+
+            char[] removeChar = remove.ToCharArray();
+            bool[] charSet = new bool[256];
+            for (int i = 0; i < 256; i++) charSet[i] = false;
+            for (int i = 0; i < removeChar.Length; i++) charSet[removeChar[i]] = true;
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = input.ElementAt(i);
+                if (!charSet[c]) sb.Append(c);
+            }
+            return sb.ToString();
+        }
+
+        public string reverseWords(string input)
+        {
+            if (input.Length < 2) return input;
+
+            StringBuilder sb = new StringBuilder();
+
+            int wordLength=0;
+            for (int i = input.Length-1; i >= 0; i--)
+            {
+                char c = input.ElementAt(i);
+                if (c == ' ')
+                {
+                    sb.Append(input.Substring(i + 1, wordLength) + ' ');
+                    wordLength = 0;
+                }
+                else wordLength++;
+            }
+            if (wordLength >= 0) sb.Append(input.Substring(0, wordLength));
+            return sb.ToString();
+        }
+
+        public int StringToInt(string input)
+        {
+            bool negative = false;
+            int number = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = input.ElementAt(i);
+                if (i == 0 && c == '-') negative = true;
+                else number = number * 10 + (c - '0');
+            }
+            if (negative) number *= -1;
+            return number;
+        }
+
+        public string IntToString(int number)
+        {
+            Stack<char> s = new Stack<char>();
+            StringBuilder sb = new StringBuilder();
+            if (number < 0)
+            {
+                number = -1 * number;
+                sb.Append("-");
+            }
+            do
+            {
+                int digit = number % 10;
+                number = number / 10;
+                s.Push((char)(digit + '0'));
+            } while (number != 0);
+            while (s.Count > 0) sb.Append(s.Pop());
+            return sb.ToString();
+        }
+    }
+}
